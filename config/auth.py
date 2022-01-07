e92c4827e0992c9e0dee34f9ccde18b7815812f7
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-01-07 14:19:57 +0545

    Use ugettext instead of ugettext_lazy

--- config/auth.py
+++ config/auth.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
-from django.utils.translation import gettext
+from django.utils.translation import gettext as _
 
-PERMISSION_DENIED_MESSAGE = 'You do not have permission to perform this action.'
+PERMISSION_DENIED_MESSAGE = _('You do not have permission to perform this action.')
 
 
 class WhiteListMiddleware:
@@ -13,5 +13,5 @@ class WhiteListMiddleware:
         # furthermore, this check must only happen in the root node, and not in deeper nodes
         if root is None:
             if not info.context.user.is_authenticated and info.field_name not in settings.GRAPHENE_NODES_WHITELIST:
-                raise PermissionDenied(gettext(PERMISSION_DENIED_MESSAGE))
+                raise PermissionDenied(PERMISSION_DENIED_MESSAGE)
         return next(root, info, **args)
--- utils/permissions.py
+++ utils/permissions.py
@@ -1,9 +1,9 @@
 from typing import List, Callable
 import logging
 from django.core.exceptions import PermissionDenied
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 
-PERMISSION_DENIED_MESSAGE = 'You do not have permission to perform this action.'
+PERMISSION_DENIED_MESSAGE = _('You do not have permission to perform this action.')
 
 logger = logging.getLogger(__name__)
 
@@ -12,7 +12,7 @@ def permission_checker(perms: List[str]) -> Callable[..., Callable]:
     def wrapped(func):
         def wrapped_func(root, info, *args, **kwargs):
             if not info.context.user.has_perms(perms):
-                raise PermissionDenied(_(PERMISSION_DENIED_MESSAGE))
+                raise PermissionDenied(PERMISSION_DENIED_MESSAGE)
             return func(root, info, *args, **kwargs)
         return wrapped_func
     return wrapped
@@ -22,7 +22,7 @@ def is_authenticated() -> Callable[..., Callable]:
     def wrapped(func):
         def wrapped_func(root, info, *args, **kwargs):
             if not info.context.user.is_authenticated:
-                raise PermissionDenied(_(PERMISSION_DENIED_MESSAGE))
+                raise PermissionDenied(PERMISSION_DENIED_MESSAGE)
             return func(root, info, *args, **kwargs)
         return wrapped_func
     return wrapped
