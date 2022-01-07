73f2c84c66018ab32bce96e49ea0e283bf9299f2
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-01-07 09:44:45 +0545

    Enable locale in admin

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,12 @@ import json
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
+from django.utils.translation import gettext_lazy as _
 from graphene_file_upload.django import FileUploadGraphQLView
 from utils.graphene.context import GQLContext
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 
 class CustomGraphQLView(FileUploadGraphQLView):
@@ -46,9 +48,11 @@ class CustomGraphQLView(FileUploadGraphQLView):
 
 CustomGraphQLView.graphiql_template = "graphene_graphiql_explorer/graphiql.html"
 
-
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
+)
+
+urlpatterns += [
     path('graphiql/', csrf_exempt(CustomGraphQLView.as_view(graphiql=True))),
     path('graphql/', csrf_exempt(CustomGraphQLView.as_view())),
 ]
@@ -56,3 +60,5 @@ urlpatterns = [
 # Static and media file urls
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+admin.site.site_header = _("Kitab Bazar administration")
