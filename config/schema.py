ef9580ccc7ffef535a9026d3fd716f181d5db855
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-01-07 09:45:29 +0545

    Update schema

--- config/schema.py
+++ config/schema.py
@@ -1,11 +1,19 @@
 import graphene
 
 from apps.user import schema as user_schema, mutations as user_mutations
+from apps.common import schema as common_schema
+from apps.institution import schema as institution_schema, mutations as institution_mutations
+from apps.publisher import schema as publisher_schema, mutations as publisher_mutations
+from apps.school import schema as school_schema, mutations as school_mutations
 
 
 # schemas
 class Query(
     user_schema.Query,
+    common_schema.Query,
+    institution_schema.Query,
+    publisher_schema.Query,
+    school_schema.Query,
     graphene.ObjectType,
 ):
     pass
@@ -14,6 +22,9 @@ class Query(
 # mutations
 class Mutation(
     user_mutations.Mutation,
+    institution_mutations.Mutation,
+    publisher_mutations.Mutation,
+    school_mutations.Mutation,
     graphene.ObjectType
 ):
     pass
