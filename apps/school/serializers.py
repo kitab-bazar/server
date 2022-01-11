15b8f91668e45891932d5c9d57dfd7471746bd4e
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-01-11 15:10:59 +0545

    Add address validations

--- apps/school/serializers.py
+++ apps/school/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.utils.translation import ugettext as _
 
 from apps.school.models import School
 
@@ -8,3 +9,14 @@ class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
         fields = '__all__'
+
+        def validate(self, data):
+            province = data['province']
+            district = data['district']
+            municipality = data['municipality']
+
+            if district.province != province:
+                raise serializers.ValidationError(_('District should be under province'))
+
+            if municipality.district != province:
+                raise serializers.ValidationError(_('Municipality should be under district'))
