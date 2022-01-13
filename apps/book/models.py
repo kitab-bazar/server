63f53f80eee5cd71d7d411507d0357395d229eaf
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-01-13 14:57:07 +0545

    Remove abstract model form tag

--- apps/book/models.py
+++ apps/book/models.py
@@ -4,10 +4,15 @@ from django.conf import settings
 from django.db import models
 
 from django.utils.translation import ugettext_lazy as _
-from apps.common.models import NameUniqueAbstractModel
 
 
-class Tag(NameUniqueAbstractModel):
+class Tag(models.Model):
+
+    name = models.CharField(
+        verbose_name=_('Name'),
+        max_length=256,
+        unique=True,
+    )
 
     def __str__(self):
         return self.name
