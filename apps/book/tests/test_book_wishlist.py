fa42cf393ba7e01f8cb2cccad78ea6e7dfa883ee
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-01-14 16:39:16 +0545

    Fix wishlist serializer and add tests

---
+++ apps/book/tests/test_book_wishlist.py
@@ -0,0 +1,58 @@
+from utils.graphene.tests import GraphQLTestCase
+from apps.book.factories import BookFactory
+from apps.user.factories import UserFactory
+from apps.publisher.factories import PublisherFactory
+
+
+class TestUser(GraphQLTestCase):
+    def setUp(self):
+        self.wish_list_mutation = '''
+            mutation Mutation($input: WishListInputType!) {
+                createWishlist(data: $input) {
+                    ok
+                    errors
+                    result {
+                      id
+                      book {
+                        id
+                      }
+                    }
+                }
+            }
+        '''
+        self.wish_list_query = '''
+            query MyQuery {
+              wishList {
+                results {
+                  id
+                  book {
+                    id
+                  }
+                }
+              }
+            }
+        '''
+        super().setUp()
+
+    def test_register_individual_user(self):
+        user = UserFactory.create()
+        self.force_login(user)
+        publisher = PublisherFactory.create()
+        book = BookFactory.create(publisher=publisher)
+        minput = {'book': book.id}
+        content = self.query_check(self.wish_list_mutation, minput=minput, okay=True)
+        book_id = content['data']['createWishlist']['result']['book']['id']
+        self.assertEqual(str(book.id), book_id)
+
+        # Should not add same books multiple times in wish list
+        self.query_check(self.wish_list_mutation, minput=minput, okay=False)
+
+        # Test should retrieve wishlist
+        content = self.query_check(self.wish_list_query)
+        self.assertEqual(content['data']['wishList']['results'][0]['book']['id'], str(book.id))
+
+        # Test should not retrieve wishlist of another user
+        user_2 = UserFactory.create()
+        self.force_login(user_2)
+        content = self.query_check(self.wish_list_query)
+        self.assertFalse(content['data']['wishList']['results'], [])
--- config/serializers.py
+++ config/serializers.py
@@ -10,20 +10,20 @@ def model_has_field(model, field):
         return False
 
 
-class CreatedUpdateBaseSerializer(serializers.Serializer):
+class CreatedUpdatedBaseSerializer(serializers.Serializer):
     created_by = serializers.PrimaryKeyRelatedField(read_only=True)
     updated_by = serializers.PrimaryKeyRelatedField(read_only=True)
 
     def create(self, validated_data):
-        if model_has_field(self.model, 'created_by'):
+        if model_has_field(self.Meta.model, 'created_by'):
             validated_data['created_by'] = self.context['request'].user
-        if model_has_field(self.model, 'modified_by'):
+        if model_has_field(self.Meta.model, 'modified_by'):
             validated_data['modified_by'] = self.context['request'].user
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        if model_has_field(self.model, 'created_by'):
+        if model_has_field(self.Meta.model, 'created_by'):
             validated_data['created_by'] = self.context['request'].user
-        if model_has_field(self.model, 'modified_by'):
+        if model_has_field(self.Meta.model, 'modified_by'):
             validated_data['modified_by'] = self.context['request'].user
         return super().create(validated_data)
