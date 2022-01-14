7b2b307b2bf38db17b9367bac43ce16e402aa762
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-01-14 15:38:39 +0545

    Create created updated base serializer

---
+++ config/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from django.core.exceptions import FieldDoesNotExist
+
+
+def model_has_field(model, field):
+    try:
+        model._meta.get_field(field)
+        return True
+    except FieldDoesNotExist:
+        return False
+
+
+class CreatedUpdateBaseSerializer(serializers.Serializer):
+    created_by = serializers.PrimaryKeyRelatedField(read_only=True)
+    updated_by = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    def create(self, validated_data):
+        if model_has_field(self.model, 'created_by'):
+            validated_data['created_by'] = self.context['request'].user
+        if model_has_field(self.model, 'modified_by'):
+            validated_data['modified_by'] = self.context['request'].user
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if model_has_field(self.model, 'created_by'):
+            validated_data['created_by'] = self.context['request'].user
+        if model_has_field(self.model, 'modified_by'):
+            validated_data['modified_by'] = self.context['request'].user
+        return super().create(validated_data)
