776d7be39a30b15997ae2522fe6f47c25e867b9d
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-01-14 14:24:48 +0545

    Improve book mock load data

--- apps/book/management/commands/load_book_data.py
+++ apps/book/management/commands/load_book_data.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from apps.common.models import Province, District, Municipality
+from apps.common.models import Municipality
 from apps.book.factories import BookFactory, TagFactory, AuthorFactory, CategoryFactory
 from apps.publisher.factories import PublisherFactory
 
@@ -9,17 +9,16 @@ class Command(BaseCommand):
     help = 'Create some fake book data'
 
     def handle(self, *args, **options):
+        if Municipality.objects.count() < 0:
+            self.stdout.write(self.style.ERROR('Please load province, district, municipality form fixtures.'))
+            return
+        municipality = Municipality.objects.first()
         # Create tags, authors can categories
         tag1, tag2, tag3 = TagFactory.create_batch(3)
         author1, author2, author3 = AuthorFactory.create_batch(3)
         category1, category2, category3 = CategoryFactory.create_batch(3)
-
-        # Get province, district, municipality, make sure geo data is loaded
-        province = Province.objects.first()
-        district = District.objects.first()
-        municipality = Municipality.objects.first()
         publisher1, publisher2, publisher3 = PublisherFactory.create_batch(
-            3, province=province, district=district, municipality=municipality
+            3, municipality=municipality
         )
 
         # Create books
@@ -34,7 +33,7 @@ class Command(BaseCommand):
         )
 
         BookFactory.create_batch(
-            20, publisher=publisher2, tags=[tag2],
-            categories=[category2], authors=[author2]
+            20, publisher=publisher3, tags=[tag3],
+            categories=[category3], authors=[author3]
         )
         self.stdout.write(self.style.SUCCESS('Books created.'))
