40a3ee841ffb2a742ca389fac4490bd0ba8eea60
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-01-27 10:04:40 +0545

    Add permissions in book mutation

--- apps/book/mutations.py
+++ apps/book/mutations.py
@@ -1,10 +1,14 @@
 import graphene
+from django.core.exceptions import PermissionDenied
+
 from utils.graphene.mutation import (
     generate_input_type_for_serializer,
     CreateUpdateGrapheneMutation,
     DeleteMutation
 )
+from config.permissions import BookPermissions
 
+from apps.user.models import User
 from apps.book.models import Book, WishList
 from apps.book.schema import BookType, WishListType
 from apps.book.serializers import BookSerializer, WishListSerializer
@@ -16,36 +20,48 @@ BookInputType = generate_input_type_for_serializer(
 )
 
 
-class CreateBook(CreateUpdateGrapheneMutation):
+class BookMutationMixin():
+    @classmethod
+    def filter_queryset(cls, qs, info):
+        if info.context.user.user_type == User.UserType.PUBLISHER.value:
+            return qs.filter(publisher=info.context.user.publisher)
+        elif info.context.user.user_type == User.UserType.ADMIN.value:
+            return qs
+        return Book.objects.none()
+
+    @classmethod
+    def check_permissions(cls, info, **_):
+        for permission in cls.permissions:
+            if not BookPermissions.check_permission(info, permission):
+                raise PermissionDenied(BookPermissions.get_permission_message(permission))
+        return False
+
+
+class CreateBook(BookMutationMixin, CreateUpdateGrapheneMutation):
     class Arguments:
         data = BookInputType(required=True)
     model = Book
     serializer_class = BookSerializer
     result = graphene.Field(BookType)
-
-    @classmethod
-    def check_permissions(cls, *args, **_):
-        return True
+    permissions = [BookPermissions.Permission.CREATE_BOOK]
 
 
-class UpdateBook(CreateUpdateGrapheneMutation):
+class UpdateBook(BookMutationMixin, CreateUpdateGrapheneMutation):
     class Arguments:
         data = BookInputType(required=True)
         id = graphene.ID(required=True)
     model = Book
     serializer_class = BookSerializer
     result = graphene.Field(BookType)
-
-    @classmethod
-    def check_permissions(cls, *args, **_):
-        return True
+    permissions = [BookPermissions.Permission.UPDATE_BOOK]
 
 
-class DeleteBook(DeleteMutation):
+class DeleteBook(BookMutationMixin, DeleteMutation):
     class Arguments:
         id = graphene.ID(required=True)
     model = Book
     result = graphene.Field(BookType)
+    permissions = [BookPermissions.Permission.DELETE_BOOK]
 
     @classmethod
     def check_permissions(cls, *args, **_):
@@ -80,7 +96,7 @@ class DeleteWishList(WishListMixin, DeleteMutation):
     class Arguments:
         id = graphene.ID(required=True)
     model = WishList
-    result = graphene.Field(BookType)
+    result = graphene.Field(WishListType)
 
     @classmethod
     def check_permissions(cls, *args, **_):
