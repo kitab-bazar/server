bee549f0cf8ea008abac47b1c5b8254082f915a9
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-01-28 09:38:41 +0545

    Fix test coverage generation logic.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,22 +2,31 @@
 
 export PYTHONUNBUFFERED=1
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR=$(dirname "$BASE_DIR")
+
 
 if [ "$CI" == "true" ]; then
-    pip3 install coverage pytest-xdist
+    pip3 install coverage
 
     set -e
     # Wait until database is ready
-    $BASE_DIR/wait-for-it.sh ${DATABASE_HOST:-db}:${DATABASE_PORT-5432}
+    wait-for-it ${DATABASE_HOST:-db}:${DATABASE_PORT-5432}
 
-    echo 'import coverage; coverage.process_startup()' > /code/sitecustomize.py
-    COVERAGE_PROCESS_START=`pwd`/.coveragerc COVERAGE_FILE=`pwd`/.coverage PYTHONPATH=`pwd` py.test -n auto --dist=loadfile --durations=10
+    # To show migration logs
+    ./manage.py test -v 2 config.tests.test_fake
+
+    # Finally run test
+    COVERAGE_PROCESS_START=`pwd`/.coveragerc COVERAGE_FILE=`pwd`/.coverage PYTHONPATH=`pwd` coverage run -m py.test --reuse-db --durations=10
 
     # Collect/Generate reports
-    coverage combine
     coverage report -i
     coverage html -i
     coverage xml
+
+    mkdir -p $ROOT_DIR/coverage/
+    mv htmlcov $ROOT_DIR/coverage/
+    mv coverage.xml $ROOT_DIR/coverage/
+
     set +e
 else
     py.test
