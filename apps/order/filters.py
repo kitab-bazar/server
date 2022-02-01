182f90c92277f52f7fd06779abef14696b73feac
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-02-01 15:07:35 +0545

    Add title filter in in book roder, add status filter in order

--- apps/order/filters.py
+++ apps/order/filters.py
@@ -1,16 +1,31 @@
 import django_filters
-from apps.order.models import BookOrder
+from apps.order.models import BookOrder, Order
+from utils.graphene.filters import StringListFilter
 
 
 class BookOrderFilterSet(django_filters.FilterSet):
+    title = django_filters.CharFilter(method='filter_title')
 
     class Meta:
         model = BookOrder
-        fields = ('id',)
+        fields = ('title',)
+
+    def filter_title(self, queryset, name, value):
+        if not value:
+            return queryset
+        return queryset.filter(title__icontains=value)
 
 
 class OrderFilterSet(django_filters.FilterSet):
+    status = StringListFilter(method='filter_order_status')
 
     class Meta:
-        model = BookOrder
-        fields = ('id',)
+        model = Order
+        fields = ('status',)
+
+    def filter_order_status(self, queryset, name, value):
+        print(value)
+        if not value:
+            return queryset
+        order_status_list = list(map(str.lower, value))
+        return queryset.filter(status__in=order_status_list)
