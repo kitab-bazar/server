dc048eaa393d5c9a30493c6b8f652f329af7ce6a
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-02-10 17:21:46 +0545

    Change email and inapp notification message

--- apps/order/tasks.py
+++ apps/order/tasks.py
@@ -10,30 +10,66 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def send_notification_to_customer(order_obj, notification_type, title):
+def get_order_data(order_obj):
+    '''
+    Prepare data to send order email
+    '''
+    return {
+        'total_price': order_obj.total_price,
+        'order_code': order_obj.order_code,
+        'status': order_obj.status,
+        'order_placed_at': order_obj.order_placed_at,
+        'order_items': [
+            {
+                'title': order_item.title,
+                'price': order_item.price,
+                'quantity': order_item.quantity,
+                'isbn': order_item.isbn,
+                'edition': order_item.edition,
+                'total_price': order_item.total_price,
+                'image': order_item.image.url if order_item.image else ''
+            } for order_item in order_obj.book_order.all()
+        ]
+    }
+
+
+def send_notification_to_customer(
+    order_obj,
+    notification_type,
+    in_app_title,
+    mail_title,
+    heading,
+):
     # Send in app notification
     Notification.objects.create(
         content_object=order_obj,
         recipient=order_obj.created_by,
         notification_type=notification_type,
-        title=title,
+        title=in_app_title,
     )
     # Send mail notification
     html_context = {
-        "heading": title,
-        "message": title,
-        "full_name": order_obj.created_by.full_name,
+        'heading': heading,
+        'message': mail_title,
+        'full_name': order_obj.created_by.full_name,
+        'order_data': get_order_data(order_obj)
     }
-    subject, message = title, title
+    subject, message = mail_title, mail_title
     transaction.on_commit(
         lambda: generic_email_sender.delay(
-            subject, message, [order_obj.created_by.email], html_context=html_context
+            subject, message, [order_obj.created_by.email], html_context=html_context, email_for='order'
         )
     )
     return True
 
 
-def send_notfication_to_publisher(order_obj, notification_type, title):
+def send_notfication_to_publisher(
+    order_obj,
+    notification_type,
+    in_app_title,
+    mail_title,
+    heading,
+):
     book_orders = order_obj.book_order.distinct('publisher')
     # NOTE: a book oder may have multiple publishers
     # Send in app notification
@@ -43,7 +79,7 @@ def send_notfication_to_publisher(order_obj, notification_type, title):
             # Assuming a publisher profile is associated with single user
             recipient=book_order.publisher.publisher_user.first(),
             notification_type=notification_type,
-            title=title,
+            title=in_app_title,
         ) for book_order in book_orders
     ])
 
@@ -57,37 +93,80 @@ def send_notfication_to_publisher(order_obj, notification_type, title):
     # Send mail notifications
     # TODO: Improve subject, message, headings of email
     html_context = {
-        "heading": title,
-        "message": title
+        'heading': heading,
+        'message': mail_title,
+        'full_name': order_obj.created_by.full_name,
+        'order_data': get_order_data(order_obj)
     }
-    subject, message = title, title
+    subject, message = mail_title, mail_title
     transaction.on_commit(
         lambda: generic_email_sender.delay(
-            subject, message, recipient_list, html_context=html_context
+            subject, message, recipient_list, html_context=html_context, email_for='order'
         )
     )
     return True
 
 
-@shared_task(name="notification_sender")
+@shared_task(name='notification_sender')
 def send_notification(order_id):
     order_obj = Order.objects.get(id=order_id)
+    notification_date_attrs = {
+        'day_name': order_obj.order_placed_at.strftime('%A'),
+        'day': order_obj.order_placed_at.day,
+        'month_name': order_obj.order_placed_at.strftime('%B'),
+        'year': order_obj.order_placed_at.year,
+        'time': order_obj.order_placed_at.strftime('%H:%M'),
+        'order_code': order_obj.order_code,
+    }
     if order_obj.status == Order.OrderStatus.RECEIVED.value:
-        title = _('Book order received.')
+        heading = 'Order received.'
+        in_app_title = _(
+            'Your Order %{order_code} placed on {day_name} {day}th {month_name} {year} at {time} has been recieved'
+        ).format(**notification_date_attrs)
+
+        mail_title = _(
+            'Your Order %{order_code} placed on {day_name} {day}th {month_name}'
+            '{year} at {time} has been recieved with details as below.'
+        ).format(**notification_date_attrs)
+
         notification_type = Notification.NotificationType.ORDER_RECEIVED.value
-        send_notfication_to_publisher(order_obj, notification_type, title)
+        send_notfication_to_publisher(order_obj, notification_type, in_app_title, mail_title, heading)
 
     elif order_obj.status == Order.OrderStatus.PACKED.value:
-        title = _('Book order packed.')
+        heading = 'Order packed.'
+        in_app_title = _(
+            'Your order %{order_code} placed on {day}th {month_name} {year} at {time} has been packed.'
+        ).format(**notification_date_attrs)
+
+        mail_title = _(
+            'Your order %{order_code} placed on {day}th {month_name}'
+            '{year} at {time} has been packed with details as below.'
+        ).format(**notification_date_attrs)
+
         notification_type = Notification.NotificationType.ORDER_PACKED.value
-        send_notification_to_customer(order_obj, notification_type, title)
+        send_notification_to_customer(order_obj, notification_type, in_app_title, mail_title, heading)
 
     elif order_obj.status == Order.OrderStatus.COMPLETED.value:
-        title = _('Book order completed.')
+        heading = 'Order completed.'
+        in_app_title = _(
+            'Your order %{order_code} placed on {day}th {month_name} {year} at {time} has been successfully delivered.'
+        ).format(**notification_date_attrs)
+
+        mail_title = _(
+            'Your order %{order_code} placed on {day}th {month_name}'
+            '{year} at {time} has been successfully delivered with details as below.'
+        ).format(**notification_date_attrs)
+
         notification_type = Notification.NotificationType.ORDER_COMPLETED.value
-        send_notification_to_customer(order_obj, notification_type, title)
+        send_notification_to_customer(order_obj, notification_type, in_app_title, mail_title, heading)
 
     elif order_obj.status == Order.OrderStatus.CANCELLED.value:
-        title = _('Book order cancelled.')
+        heading = 'Order cancelled.'
+        in_app_title = _(
+            'Your Order %{order_code} has been successfully cancelled.'
+        ).format(**notification_date_attrs)
+        mail_title = _(
+            'Your Order %{order_code} has been successfully cancelled.'
+        ).format(**notification_date_attrs)
         notification_type = Notification.NotificationType.ORDER_CANCELLED.value
-        send_notification_to_customer(order_obj, notification_type, title)
+        send_notification_to_customer(order_obj, notification_type, in_app_title, mail_title, heading)
