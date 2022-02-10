725590195a8823da04e7cf3b38b45d43af17746f
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-02-10 17:21:25 +0545

    Separate order notification form general notification

--- apps/common/tasks.py
+++ apps/common/tasks.py
@@ -5,15 +5,19 @@ from django.conf import settings
 
 
 @shared_task(name="generic_email_sender")
-def generic_email_sender(subject, message, recipient, html_context=None):
+def generic_email_sender(subject, message, recipient, html_context=None, email_for=None):
     email_data = {
         "subject": subject,
         "message": message,
         "from_email": settings.DEFAULT_FROM_EMAIL,
         "recipient_list": recipient,
     }
-    if html_context:
+    if html_context and email_for is None:
         email_data["html_message"] = render_to_string(
             "emails/generic_email.html", html_context
+        )
+    elif html_context and email_for == 'order':
+        email_data["html_message"] = render_to_string(
+            "emails/order_email.html", html_context
         )
     send_mail(**email_data)
