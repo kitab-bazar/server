35febe0ce1169ea3e6fed23a7e2e92b7f474e956
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-02-15 12:47:46 +0545

    Remove custom filter

--- config/settings.py
+++ config/settings.py
@@ -114,10 +114,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
-            "libraries": {
-                "custom_tags": "utils.custom_tags",
-            }
+            ]
         },
     },
 ]
