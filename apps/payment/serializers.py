4526ba2ab22f24e4a9eeaf1356492f276feb4007
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-02-25 16:13:59 +0545

    Add payment log data in payment

--- apps/payment/serializers.py
+++ apps/payment/serializers.py
@@ -1,19 +1,48 @@
 from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
 
-from apps.payment.models import Payment
+from apps.payment.models import Payment, PaymentLog
+from config.serializers import CreatedUpdatedBaseSerializer
 
 
-class PaymentSerializer(serializers.ModelSerializer):
+class PaymentLogSerializer(CreatedUpdatedBaseSerializer, serializers.ModelSerializer):
+
+    class Meta:
+        model = PaymentLog
+        fields = ('comment', 'files')
+
+    def validate_files(self, files):
+        for file in files:
+            if self.context['request'].user != file.created_by:
+                raise serializers.ValidationError(_('Invalid file'))
+        return files
+
+
+class PaymentSnapshotSerializer(CreatedUpdatedBaseSerializer, serializers.ModelSerializer):
     class Meta:
         model = Payment
-        exclude = ('created_by', 'modified_by',)
+        fields = ('status',)
+
+
+class PaymentSerializer(CreatedUpdatedBaseSerializer, serializers.ModelSerializer):
+    payment_log = PaymentLogSerializer(required=False)
+
+    class Meta:
+        model = Payment
+        fields = '__all__'
 
     def create(self, validated_data):
-        validated_data['created_by'] = self.context['request'].user
-        validated_data['modified_by'] = self.context['request'].user
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        validated_data['created_by'] = self.context['request'].user
-        validated_data['modified_by'] = self.context['request'].user
-        return super().update(instance, validated_data)
+        payment_log_data = validated_data.pop('payment_log', None)
+        payment = super().create(validated_data)
+        snapshot_serializer = PaymentSnapshotSerializer(instance=payment)
+        if payment_log_data:
+            files = payment_log_data.pop('files', None)
+            payment_log = PaymentLog.objects.create(
+                payment=payment,
+                snapshot=snapshot_serializer.data,
+                **payment_log_data,
+                created_by=self.context['request'].user
+            )
+            if files:
+                payment_log.add(*files)
+        return payment
