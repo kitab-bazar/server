15b6053449cf37b50e28eb8b27337dee9f3f2a9f
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-03-14 13:32:43 +0545

    Fix forget password validation error

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -256,7 +256,7 @@ class GenerateResetPasswordTokenSerializer(serializers.Serializer):
         # if no user exists for this email
         except User.DoesNotExist:
             # explanatory email message
-            raise serializers.ValidationError(gettext('Invalid Credentials'))
+            raise serializers.ValidationError(gettext('User with this email does not exists'))
         subject = gettext("Reset password")
         html_context = {
             "heading": gettext("Reset Password"),
