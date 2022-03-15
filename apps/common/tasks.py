52345d18f69ec24e4d7a03c9fc80d67b5b024fc7
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-03-15 14:29:53 +0545

    Fix generic email sender

--- apps/common/tasks.py
+++ apps/common/tasks.py
@@ -12,8 +12,8 @@ def generic_email_sender(subject, message, recipient, html_context=None):
         "message": message,
         "from_email": settings.DEFAULT_FROM_EMAIL,
         "recipient_list": recipient,
-        "footer_message": _("If you are having trouble with the button above, copy and paste the URL below into your web browser.") # noqa E501
     }
+    html_context['footer_message'] = _("If you are having trouble with the button above, copy and paste the URL below into your web browser.") # noqa E501]
     if html_context:
         email_data["html_message"] = render_to_string(
             "emails/generic_email.html", html_context
