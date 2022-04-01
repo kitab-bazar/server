663b75115c4f2be3cba5362d5caca275a5b0cf9b
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-04-01 15:44:09 +0545

    Add paid by user filter

--- apps/payment/filter_set.py
+++ apps/payment/filter_set.py
@@ -1,6 +1,6 @@
 import django_filters
 
-from utils.graphene.filters import SimpleInputFilter
+from utils.graphene.filters import SimpleInputFilter, IDListFilter
 from apps.payment.models import Payment, PaymentLog
 from apps.payment.enums import (
     StatusEnum,
@@ -13,10 +13,16 @@ class PaymentFilterSet(django_filters.FilterSet):
     status = SimpleInputFilter(StatusEnum)
     transaction_type = SimpleInputFilter(TransactionTypeEnum)
     payment_type = SimpleInputFilter(PaymentTypeEnum)
+    paid_by_users = IDListFilter(method='filter_paid_by_users')
 
     class Meta:
         model = Payment
         fields = ()
+
+    def filter_paid_by_users(self, queryset, name, value):
+        if not value:
+            return queryset
+        return queryset.filter(paid_by__in=value)
 
 
 class PaymentLogFilterSet(django_filters.FilterSet):
