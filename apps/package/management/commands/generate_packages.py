3182feb0e6b1269b8a72061b08b93b964b5a2cf2
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-04-25 09:12:20 +0545

    Change package generation process

--- apps/package/management/commands/generate_packages.py
+++ apps/package/management/commands/generate_packages.py
@@ -1,6 +1,7 @@
-from django.conf import settings
+import io
 from django.core.files.base import File
-import csv
+from openpyxl import Workbook
+from openpyxl.writer.excel import save_virtual_workbook
 from django.db import transaction
 from django.contrib.postgres.aggregates import StringAgg
 from django.core.management.base import BaseCommand
@@ -17,8 +18,9 @@ from apps.package.models import (
 )
 from apps.user.models import User
 from apps.common.models import Municipality
+from apps.package.seed.incentive import INCENTIVE_BOOKS
 
-INCENTIVE_LIMIT = 20
+INCENTIVE_LIMIT = 10
 
 
 class Command(BaseCommand):
@@ -44,28 +46,103 @@ class Command(BaseCommand):
                 user['id'], user['full_name']) for user in unverified_user_qs]
         )
 
-    def _generate_related_orders_export(self, package, related_book_orders):
-        filename = 'book_orders.csv'
-        with open(f'{settings.TEMP_DIR}/{filename}', 'w') as file:
-            writer = csv.writer(file)
-            writer.writerow([
-                'Package id', "Order code", "Book name", "Book author", "Book quantity", "Book Grade",
-                "Book ISBN", "Book edition", "Book language", "Book price"
+    def _generate_related_orders_export(
+        self, package,
+        related_book_orders,
+        total_school_book_quantity_list,
+    ):
+
+        def _append_or_update_incentive(incentive_list, book, publisher_id):
+            if incentive_list:
+                for kitab_incentive in incentive_list:
+                    if (
+                        book['book_name'] == kitab_incentive.get('book_name') and
+                        book['publisher_name'] == kitab_incentive.get('publisher_name') and
+                        book['publisher_id'] == publisher_id
+                    ):
+                        # Increment quantity by one
+                        kitab_incentive['quantity'] += 1
+                        return kitab_incentive
+            return incentive_list.append(book)
+
+        filename = f'{package.publisher.name}book_orders.xlsx'
+        wb = Workbook(filename)
+        ws = wb.active # noqa
+        ws1 = wb.create_sheet("Book orders")
+        ws1.append([
+            "Package Id", "Book Name", "Book Author/Authors", "Book Grade", "Book ISBN",
+            "Book Edition", "Book Language", "Book Quantity", "Unit Book price",
+            "Sub Total Book price"
+        ])
+
+        # To add sum formula at bottom
+        book_grand_total = 0
+        for book_order in related_book_orders:
+            ws1.append([
+                str(package.package_id),
+                book_order['book__title'] if book_order['book__title'] else "",
+                book_order['book_authors'] if book_order['book_authors'] else "",
+                book_order['book__grade'] if book_order['book__grade'] else "",
+                book_order['book__isbn'] if book_order['book__isbn'] else "",
+                book_order['book__edition'] if book_order['book__edition'] else "",
+                book_order['book__language'] if book_order['book__language'] else "",
+                book_order['total_quantity'] if book_order['total_quantity'] else 0,
+                book_order['book__price'] if book_order['total_quantity'] else 0,
+                book_order['total_price'] if book_order['total_price'] else 0
             ])
-            for book_order in related_book_orders:
-                writer.writerow([
-                    package.package_id, book_order['order__order_code'], book_order['book__title'],
-                    book_order['book_authors'], book_order['quantity'], book_order['book__grade'],
-                    book_order['isbn'], book_order['edition'], book_order['book__language'], book_order['price']
-                ])
-        csv_file = open(file.name, 'r')
-        package.orders_export_file.save('book_orders.csv', File(csv_file))
-        csv_file.close()
+            book_grand_total += book_order['total_quantity'] * book_order['book__price']
+        ws1.append([
+            "", "", "", "", "", "", "", "", "Grand Total Price", book_grand_total
+        ])
+        incentive_list = []
+        ws2 = wb.create_sheet("Incentive Orders")
+        ws2.append([
+            "Book Name", "Unit Price", "Quantity", "Sub Total Price"
+        ])
+        self.stdout.write(self.style.SUCCESS(f'{list(total_school_book_quantity_list)} \n\n'))
+        for total_school_book_quantity in total_school_book_quantity_list:
+            # A school can can aximum 120 incentives
+            if total_school_book_quantity >= INCENTIVE_LIMIT:
+                if total_school_book_quantity > 30:
+                    total_school_book_quantity = 120
+                else:
+                    # If quantity is less then 30 school gets incentive in 1:4 ratio
+                    total_school_book_quantity = total_school_book_quantity * 4
+
+                if total_school_book_quantity > 0:
+                    incentive_key = f'book_list_{total_school_book_quantity}'
+                if incentive_key:
+                    for book in INCENTIVE_BOOKS[incentive_key]:
+                        _append_or_update_incentive(incentive_list, book, package.publisher.id)
+
+        # To add sum formula at bottom
+        book_incentive_grand_total = 0
+        for incentive in incentive_list:
+            if incentive['publisher_id'] == package.publisher.id:
+                sub_total = incentive['price'] * incentive['quantity']
+                ws2.append(
+                    [
+                        incentive['book_name'], incentive['price'],
+                        incentive['quantity'], sub_total
+                    ]
+                )
+                book_incentive_grand_total += sub_total
+        ws2.append([
+            "", "", "Grand Total Price", book_incentive_grand_total
+        ])
+        fileToUpload = io.BytesIO(save_virtual_workbook(wb))
+        package.orders_export_file.save(filename, File(fileToUpload))
 
     def _create_publihser_packages(self, latest_order_window, orders):
         # ------------------------------------------------------------------
         # Create publihser packages
         # ------------------------------------------------------------------
+
+        # Incentive calculation should be done before filtering publishers
+        # We calculate incentives based on school orders
+        total_school_book_quantity_list = orders.values('created_by').annotate(
+            grand_total_quantity=Sum('book_order__quantity')
+        ).values_list('grand_total_quantity', flat=True)
 
         # Get unique publishers in order
         publisher_ids = orders.values_list('book_order__publisher', flat=True).distinct()
@@ -80,11 +157,15 @@ class Command(BaseCommand):
                 order__in=related_orders, publisher=publisher
             ).values('book').annotate(
                 total_quantity=Sum('quantity'),
-                total_price=Sum('quantity') * F('price'),
+                total_price=Sum(F('quantity') * F('price')),
                 book_authors=StringAgg('book__authors__name', distinct=True, delimiter=", "),
-            ).values(
-                'book', 'total_quantity', 'total_price', 'book_authors', 'book__title',
-                'quantity', 'book__grade', 'isbn', 'edition', 'book__language', 'price', 'order__order_code'
+                book__id=F('book__id'),
+                book__title=F('book__title'),
+                book__grade=F('book__grade'),
+                book__language=F('book__language'),
+                book__isbn=F('book__isbn'),
+                book__price=F('book__price'),
+                book__edition=F('book__edition'),
             )
             package = PublisherPackage.objects.create(
                 status=PublisherPackage.Status.PENDING.value,
@@ -97,11 +178,11 @@ class Command(BaseCommand):
             )
             package.related_orders.set(related_orders)
             # Generate related orders export file
-            self._generate_related_orders_export(package, related_book_orders)
+            self._generate_related_orders_export(package, related_book_orders, total_school_book_quantity_list)
             PublisherPackageBook.objects.bulk_create(
                 [
                     PublisherPackageBook(
-                        book_id=related_book_order['book'],
+                        book_id=related_book_order['book__id'],
                         quantity=related_book_order['total_quantity'],
                         publisher_package=package
                     ) for related_book_order in related_book_orders
@@ -160,8 +241,8 @@ class Command(BaseCommand):
                 'created_by__institution__municipality'
             ).annotate(
                 total_quantity=Sum('book_order__quantity'),
-                total_price=Sum('book_order__quantity') * F('book_order__price')
-            ).values('total_quantity', 'total_price')
+                total_price=Sum(F('book_order__quantity') * F('book_order__price'))
+            )
 
             CourierPackage.objects.create(
                 status=CourierPackage.Status.PENDING.value,
@@ -192,7 +273,7 @@ class Command(BaseCommand):
             related_orders = orders.filter(created_by=school_user)
             related_book_orders = BookOrder.objects.filter(order__in=related_orders).values('book__created_by').annotate(
                 total_quantity=Sum('quantity'),
-                total_price=Sum('quantity') * F('price')
+                total_price=Sum(F('quantity') * F('price'))
             ).values('book', 'total_quantity', 'total_price')
             courier_package = CourierPackage.objects.get(
                 order_window=latest_order_window, municipality=school_user.school.municipality
@@ -245,8 +326,9 @@ class Command(BaseCommand):
             related_orders = orders.filter(created_by=institution_user)
             related_book_orders = BookOrder.objects.filter(order__in=related_orders).values('book__created_by').annotate(
                 total_quantity=Sum('quantity'),
-                total_price=Sum('quantity') * F('price')
-            ).values('book', 'total_quantity', 'total_price')
+                total_price=Sum(F('quantity') * F('price')),
+                book=F('book')
+            )
             courier_package = CourierPackage.objects.get(
                 order_window=latest_order_window, municipality=institution_user.institution.municipality
             )
@@ -338,8 +420,9 @@ class Command(BaseCommand):
         # Check if packages are already created
         if (
             PublisherPackage.objects.filter(order_window=latest_order_window).exists() or
-            PublisherPackage.objects.filter(order_window=latest_order_window).exists() or
-            PublisherPackage.objects.filter(order_window=latest_order_window).exists()
+            SchoolPackage.objects.filter(order_window=latest_order_window).exists() or
+            CourierPackage.objects.filter(order_window=latest_order_window).exists() or
+            InstitutionPackage.objects.filter(order_window=latest_order_window).exists()
         ):
             f'Packages for order window {latest_order_window} are already created.'
 
